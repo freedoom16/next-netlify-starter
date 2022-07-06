afd50e8ee3a5008cd9213fb9c76eaecead3e28f3
netsanet <nmaaa007@gmail.com>
2022-07-06 13:45:48 +0300

    update

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
-import '@styles/globals.css'
+import Navbar from '../components/Navbar';
+import '../styles/globals.css';
 
-function Application({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Navbar />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default Application
+export default MyApp;
--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,23 @@
 import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
+import About from '../components/About'
+import Contact from '../components/Contact'
+import Main from '../components/Main'
+import Projects from '../components/Projects'
+import Skills from '../components/Skills'
+
 
 export default function Home() {
   return (
-    <div className="container">
+    <div>
       <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>Netsanet |software Developer</title>
+        <meta name="description" content="I’m a front-end web developer specializing in building (and occasionally designing) exceptional digital experiences." />
       </Head>
-
-      <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-      </main>
-
-      <Footer />
+    <Main />
+    <About />
+    <Skills />
+    <Projects />
+    <Contact />
     </div>
   )
 }
